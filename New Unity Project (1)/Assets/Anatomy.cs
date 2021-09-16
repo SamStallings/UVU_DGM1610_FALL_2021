cb9d738fffccbdb22aef44648ac36f2caeddca96
Sam Stallings <10915865@my.uvu.edu>
2021-09-15 18:36:40 -0600

    Added two scripts
    scripts include Anatomy and VarsAndInsp

---
+++ New Unity Project (1)/Assets/Anatomy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anatomy : MonoBehaviour
+{
+    // Use this for initialization
+    void Start ()
+    {
+        Debug.Log("I'm Alive!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
