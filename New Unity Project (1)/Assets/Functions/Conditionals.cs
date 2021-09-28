9b6f72690ea652d4887ec22c8bf318149bff0f12
Sam Stallings <10915865@my.uvu.edu>
2021-09-27 20:15:50 -0600

    start of conditionals practice

---
+++ New Unity Project (1)/Assets/Functions/Conditionals.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+    public class Conditionals : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            int x = 50;
+            int y = 10;
+            if (x > y)
+            {
+            Console.WriteLine("Hello World");
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
